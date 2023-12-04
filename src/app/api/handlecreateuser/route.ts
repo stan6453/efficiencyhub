a68a84a70c774eccd690300801b8b8ef4e0be5b5
Stanley Ezechukwu <stanley6453@gmail.com>
2023-12-04 17:44:37 +0100

    properly setup mongoose in next.js environment

--- src/app/api/handlecreateuser/route.ts
+++ src/app/api/handlecreateuser/route.ts
@@ -1,6 +1,6 @@
 import User from "../../../../mongoose/usermodel";
 export async function POST(request: Request) {
-    const body = await request.json()
+    // const body = await request.json()
     const newUser = {
         _id: 'body.data.id',
         username: 'body.data.username',
