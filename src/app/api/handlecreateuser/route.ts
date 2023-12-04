848fab99e72e92f8cb23580cd4f5cb1fbae0d619
Stanley Ezechukwu <stanley6453@gmail.com>
2023-12-04 17:56:26 +0100

    properly setup mongoose in next.js environment

--- src/app/api/handlecreateuser/route.ts
+++ src/app/api/handlecreateuser/route.ts
@@ -1,19 +1,21 @@
 import User from "../../../../mongoose/usermodel";
 export async function POST(request: Request) {
-    // const body = await request.json()
-    const newUser = {
-        _id: 'body.data.id',
-        username: 'body.data.username',
-    }
 
-    try {
-        await new User(newUser).save()
-    } catch (err) {
-        console.log(err)
-        return Response.json({error:`${err}`})
-    }
+    const body = await request.json()
+    console.log(body)
+    // const newUser = {
+    //     _id: 'body.data.id',
+    //     username: 'body.data.username',
+    // }
 
-    return Response.json(newUser);
+    // try {
+    //     await new User(newUser).save()
+    // } catch (err) {
+    //     console.log(err)
+    //     return Response.json({error:`${err}`})
+    // }
+
+    // return Response.json(newUser);
 }
 
 export const runtime = 'nodejs';
